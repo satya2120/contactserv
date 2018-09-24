68e518d3edad2475d994465a856cecd5bf800e1c
Plivo <plivo@Kunal-2.local>
2018-09-24 11:50:49 +0530

    Added basic project settings for contactserv

---
+++ src/main/java/com/plivo/contactserv/ContactservApplication.java
@@ -0,0 +1,12 @@
+package com.plivo.contactserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactservApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactservApplication.class, args);
+    }
+}
---
+++ src/test/java/com/plivo/contactserv/ContactservApplicationTests.java
@@ -0,0 +1,16 @@
+package com.plivo.contactserv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContactservApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
